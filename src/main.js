827fa625f5173b5d3eb6a39d1c1aa593e9c5bcad
Anousone Kaseumsouk, Jean Jia and Lan Dam <jean@>
2018-08-08 16:25:37 -0700

    move youtubeapiframe to global

--- src/main.js
+++ src/main.js
@@ -1,3 +1,39 @@
+var tag = document.createElement('script');
+console.log(tag);
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'M7lc1UVf-VE',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+
 $(document).ready(function() {
   $("form#artistSearch-form").submit(function(event) {
     event.preventDefault();
@@ -40,27 +76,27 @@ $(document).ready(function() {
   });
 
   let play = function(){
-    var tag = document.createElement('script');
-    console.log(tag);
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    // 3. This function creates an <iframe> (and YouTube player)
-    //    after the API code downloads.
-    var player;
-    function onYouTubeIframeAPIReady() {
-      player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'M7lc1UVf-VE',
-        events: {
-          'onReady': onPlayerReady,
-          'onStateChange': onPlayerStateChange
-        }
-      });
-    }
+    // var tag = document.createElement('script');
+    // console.log(tag);
+    //
+    // tag.src = "https://www.youtube.com/iframe_api";
+    // var firstScriptTag = document.getElementsByTagName('script')[0];
+    // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    //
+    // // 3. This function creates an <iframe> (and YouTube player)
+    // //    after the API code downloads.
+    // var player;
+    // function onYouTubeIframeAPIReady() {
+    //   player = new YT.Player('player', {
+    //     height: '390',
+    //     width: '640',
+    //     videoId: 'M7lc1UVf-VE',
+    //     events: {
+    //       'onReady': onPlayerReady,
+    //       'onStateChange': onPlayerStateChange
+    //     }
+    //   });
+    // }
 
     // 4. The API will call this function when the video player is ready.
     function onPlayerReady(event) {
