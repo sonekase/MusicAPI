aa73db0a9eefd612f824554ebc65272e22aae71e
Anousone Kaseumsouk, Jean Jia and Lan Dam <jean@>
2018-08-08 14:28:35 -0700

    add code to display lyric of song when title is selected

--- src/main.js
+++ src/main.js
@@ -1,26 +1,64 @@
 $(document).ready(function() {
-  let artistName = 'Justin Bieber';
-  let trackName;
-  $.get(`http://theaudiodb.com/api/v1/json/1/searchalbum.php?s=${artistName}`).then(function(response) {
-    let albumId = response.album[0].idAlbum;
-
-    $.get(`http://theaudiodb.com/api/v1/json/1/track.php?m=${albumId}`).then(function(response) {
-      trackName = response.track[0].strTrack;
-      $.get(`https://api.lyrics.ovh/v1/${artistName}/${trackName}`).then(function(response){
-        console.log(response.lyrics);
-        let lyricArrays = response.lyrics.replace( /\n/g, "." ).split( "." );
-        lyricArrays.forEach(function(line){
-          $(".lyrics").append(line + "<br>");
+  $("form#artistSearch-form").submit(function(event) {
+    event.preventDefault();
+    $(".tracklist").show();
+    $(".lyrics").text("");
+    $("#title").text("");
+    let artistName = $("#artistName").val();
+    let albumId, trackName;
+    let trackLists = [];
+    $(".tracklist").text("");
+    $.get(`http://theaudiodb.com/api/v1/json/1/searchalbum.php?s=${artistName}`).then(function(response) {
+      response.album.forEach(function(album) {
+        albumId = album.idAlbum;
+        $.get(`http://theaudiodb.com/api/v1/json/1/track.php?m=${albumId}`).then(function(response) {
+          response.track.forEach(function(track) {
+            let title = track.strTrack;
+            trackLists.push(track.strTrack);
+            $(".tracklist").append(`<p class="findLyric">${title}</p>`);
+            $(".findLyric").last().click(function() {
+              $(".tracklist").hide();
+              $.get(`https://api.lyrics.ovh/v1/${artistName}/${title}`).then(function(response){
+                $("#title").text(title);
+                let lyricArrays = response.lyrics.replace( /\n/g, "." ).split( "." );
+                lyricArrays.forEach(function(line){
+                  $(".lyrics").append(line + "<br>");
+                });
+              }).fail(function(error) {
+                $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+              });
+            })
+          });
+        }).fail(function(error) {
+          $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
         });
-      }).fail(function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
       });
     }).fail(function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
-  }).fail(function(error) {
-    $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
   });
+
+
+  // $.get(`http://theaudiodb.com/api/v1/json/1/searchalbum.php?s=${artistName}`).then(function(response) {
+  //   let albumId = response.album[0].idAlbum;
+  //
+  //   $.get(`http://theaudiodb.com/api/v1/json/1/track.php?m=${albumId}`).then(function(response) {
+  //     trackName = response.track[0].strTrack;
+      // $.get(`https://api.lyrics.ovh/v1/${artistName}/${trackName}`).then(function(response){
+      //   console.log(response.lyrics);
+      //   let lyricArrays = response.lyrics.replace( /\n/g, "." ).split( "." );
+      //   lyricArrays.forEach(function(line){
+      //     $(".lyrics").append(line + "<br>");
+      //   });
+      // }).fail(function(error) {
+      //   $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+      // });
+  //   }).fail(function(error) {
+  //     $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+  //   });
+  // }).fail(function(error) {
+  //   $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+  // });
 
   // var request = new XMLHttpRequest();
   // request.open('GET', `https://api.lyrics.ovh/v1/Drake/The Motto`);
